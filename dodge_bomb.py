0f125220a7ba1e09e64c5210bc564512603eefcd
c0a24034af <c0a24034af@edu.teu.ac.jp>
2025-04-22 17:22:20 +0900

    演習1 ゲームオーバー画面

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
@@ -27,7 +28,36 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
      if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
      return yoko, tate
-    
+
+def gameover(screen: pg.Surface) -> None:
+    """
+     黒い画面を描画
+     文字列"Game Over"を表示
+     こうかとん2体を表示
+     """
+    bl_img = pg.Surface((WIDTH*2,HEIGHT*2)) #黒い画面を描画
+    bl_img.set_alpha(200)
+    pg.draw.rect(bl_img,(0, 0, 0),pg.Rect(0,0,WIDTH,HEIGHT))
+    bl_rct = bl_img.get_rect()
+    bl_rct.center = (WIDTH,HEIGHT)
+    screen.blit(bl_img,bl_rct)
+
+    fonto = pg.font.Font(None, 50) # 文字列表示
+    txt = fonto.render("Game Over",True, (255, 255, 255))
+    screen.blit(txt, [450,310])
+
+    kk_img1 = pg.image.load("fig/8.png") # こうかとん(左)表示 
+    kk_img1 = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    kk_rct1 = kk_img1.get_rect()
+    kk_rct1.center = 380, 320
+    screen.blit(kk_img1, kk_rct1)
+    kk_img2 = pg.image.load("fig/8.png")    
+    kk_img2 = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    kk_rct2 = kk_img1.get_rect()
+    kk_rct2.center = 700, 320
+    screen.blit(kk_img2, kk_rct2)
+    pg.display.update()
+    time.sleep(5)
 
 
 def main():
@@ -51,24 +81,31 @@ def main():
                 return
         screen.blit(bg_img, [0, 0])
 
-        if kk_rct.colliderect(bb_rct):
+        if kk_rct.colliderect(bb_rct): 
+        #  こうかとんが爆弾に当たったときに関数gameover()を実行
+            gameover(screen)
             return
-        key_lst = pg.key.get_pressed()
+            
+            
+
+        key_lst = pg.key.get_pressed() # こうかとん移動
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
                 if key_lst[key]:
                      sum_mv[0] += mv[0] 
                      sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):
+
+        if check_bound(kk_rct) != (True, True): # 画面判定
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1]) # 画面内に戻す
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
-        screen.blit(kk_img, kk_rct)
-        screen.blit(bb_img,bb_rct)
+
+        screen.blit(kk_img, kk_rct) # こうかとん描画
+        screen.blit(bb_img,bb_rct) #爆弾描画
         bb_rct.move_ip(vx,vy)
         pg.display.update()
         tmr += 1
